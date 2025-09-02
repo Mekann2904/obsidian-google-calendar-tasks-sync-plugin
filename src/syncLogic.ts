a091954c3756f796e5d30d85272f61cae35247b5
Mekann2904 <e235737@ie.u-ryukyu.ac.jp>
2025-09-02 14:27:28 +0900

    Add performance import in sync logic

--- src/syncLogic.ts
+++ src/syncLogic.ts
@@ -1,3 +1,4 @@
+import { performance } from 'perf_hooks';
 import { Notice } from 'obsidian';
 import { ErrorHandler, DateUtils, FingerprintUtils } from './commonUtils';
 import { rrulestr } from 'rrule';
